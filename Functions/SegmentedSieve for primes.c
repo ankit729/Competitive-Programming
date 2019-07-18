774b7962de9f81c5ae1c6a1bfd24cb96f71c524a
ankit729 <36035184+ankit729@users.noreply.github.com>
2019-07-18 18:31:30 +0530

    Update SegmentedSieve for primes.c

--- Functions/SegmentedSieve for primes.c
+++ Functions/SegmentedSieve for primes.c
@@ -5,18 +5,15 @@
 #define ll long long int
 ll psize=0,psize2;
 ll prime[1000000];
-int ppush(p)
-{
+int ppush(p){
     prime[psize++]=p;
     return 0;
 }
-int ppush2(p)
-{
+int ppush2(p){
     prime[psize2++]=p;
     return 0;
 }
-void simpleSieve(ll limit)
-{
+void simpleSieve(ll limit){
     ll mark[limit+1];
     for(ll i=0;i<limit+1;i++)
         mark[i]=1;
@@ -28,22 +25,19 @@ void simpleSieve(ll limit)
         if(mark[p]==1)
             ppush(p);
 }
-void segmentedSieve(ll n)
-{
+void segmentedSieve(ll n){
     ll limit=1000000;
     simpleSieve(limit);
     psize2=psize;
     ll low=limit;
     ll high=2*limit;
-    while(low<n)
-    {
+    while(low<n){
         if(high>=n)
            high=n;
         ll mark[limit+1];
         for(ll i=0;i<limit+1;i++)
             mark[i]=1;
-        for(ll i=0;i<psize;i++)
-        {
+        for(ll i=0;i<psize;i++){
             ll loLim=floor(low/prime[i])*prime[i];
             if(loLim<low)
                 loLim+=prime[i];
@@ -57,8 +51,7 @@ void segmentedSieve(ll n)
         high=high+limit;
     }
 }
-int main()
-{
+int main(void){
     ll n=10000000;
     segmentedSieve(n);
     return 0;
