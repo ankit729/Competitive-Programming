a428109505c2cc35c625e8072797862652749af8
ankit729 <36035184+ankit729@users.noreply.github.com>
2019-08-11 23:41:04 +0530

    Update sieve of eratosthenes.c

--- Functions/sieve of eratosthenes.c
+++ Functions/sieve of eratosthenes.c
@@ -2,17 +2,23 @@
 #include<stdlib.h>
 #include<stdbool.h>
 #define ll long long int
+int sieve_of_eratosthenes(int limit,bool prime[limit]){
+	memset(prime,true,(limit+1)*sizeof(bool));
+	prime[0]=prime[1]=false;
+	for(ll i=4;i<=limit;i+=2)
+		prime[i]=false;
+	for(ll i=3;i*i<=limit;i+=2)
+		if(prime[i]==true)
+			for(ll j=i*i;j<=limit;j+=i)
+				prime[j]=false;
+	for(ll i=0;i<=limit;i++)
+		if(prime[i])
+			printf("%lld\t",i);
+}
 int main(void){
 	ll limit;
 	scanf("%lld",&limit);
 	bool prime[limit+1];
-	memset(prime,true,sizeof(prime));
-	prime[0]=prime[1]=false;
-	for(ll j=4;j<=limit;j+=2)
-		prime[j]=!prime[j];
-	for(ll i=3;i*i<=limit;i+=2)
-		if(prime[i]==true)
-			for(ll j=i*i;j<=limit;j+=i)
-				prime[j]=!prime[j];
+	sieve_of_eratosthenes(limit,prime);
 	return 0;
 }
