56621261fe319f80a13227f3e7ab478e2e324c98
ankit729 <36035184+ankit729@users.noreply.github.com>
2019-08-14 21:27:19 +0530

    Update power.c

--- Functions/power.c
+++ Functions/power.c
@@ -2,8 +2,8 @@
 #define ull unsigned long long int
 #define ll long long int
 #define MOD 1000000007
-ull power2(ull x,ull y,ull m){
-    ull res=1;
+ll power(ll x,ll y,ll m){
+    ll res=1;
     x=x%m;
     while(y>0){
         if(y&1)
@@ -13,12 +13,12 @@ ull power2(ull x,ull y,ull m){
     }
     return res;
 }
-ull power3(ull a,ull b,ull c,ull m){
-    ull ans=power2(b,c,m-1);
-    return power2(a,ans,m);
+ll power3(ll a,ll b,ll c,ll m){
+    ll ans=power(b,c,m-1);
+    return power(a,ans,m);
 }
 int main(void){
-	ull a=2,b=3,c=4;
+	ll a=2,b=3,c=4;
 	power3(a,b,c,MOD);
 	return 0;
 }
