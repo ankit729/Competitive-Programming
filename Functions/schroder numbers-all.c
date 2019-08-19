3000436eb61aceae7b529a978aa0e3ed93e2c80d
ankit729 <36035184+ankit729@users.noreply.github.com>
2019-08-19 16:43:09 +0530

    Update schroder numbers-all.c

--- Functions/schroder numbers-all.c
+++ Functions/schroder numbers-all.c
@@ -6,8 +6,12 @@ ll schroder(ll n){
     m[1]=2;
     for(ll i=2;i<=n;i++){
         m[i]=3*m[i-1];
-        for(ll j=1;j<=i-2;j++)
-            m[i]+=m[j]*m[i-j-1];
+        ll j;
+        for(j=1;j<=i-j-1;j++)
+            m[i]+=2*m[j]*m[i-j-1];
+        j--;
+        if(j==i-j-1)
+            m[i]-=m[j]*m[i-j-1];
     }
     return m[n];
 }
