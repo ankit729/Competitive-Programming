6e9fbcda2a75edf7668482a40d1499b2d746502f
ankit729 <36035184+ankit729@users.noreply.github.com>
2019-10-11 15:57:13 +0530

    Update count setbits (range).c

--- Functions/count setbits (range).c
+++ Functions/count setbits (range).c
@@ -2,18 +2,25 @@
 #include<math.h>
 #define ll long long int
 #define integer_size 32
+ll setbits[integer_size+1];
 ll countBitsUtil(ll a,ll b){
-    if(a==1 && b==1)
-        return 1;
-    if(b==0)
-        return 0;
-    int size=log2(b)+1;
-    ll mid=1<<(size-1);
-    if(a<mid)
-        return b-mid+1+countBitsUtil(a,mid-1)+countBitsUtil(1,b-mid);
-    if(a==mid)
-        return b-a+1+countBitsUtil(1,b-mid);
-    return b-a+1+countBitsUtil(a-mid,b-mid);
+    if(b<=1)
+        return b;
+    ll sa=1+log2(a);
+    ll sb=1+log2(b);
+    if(sa<sb){
+        ll x,y,z;
+        ll ma=(1<<(sa-1));
+        x=(a&(a-1)==0)?setbits[sa]-setbits[sa-1]:(ma<<1)-a+countBitsUtil(a-ma,ma-1);
+        y=setbits[sb-1]-setbits[sa];
+        ll mb=(1<<(sb-1));
+        z=(b&(b+1)==0)?setbits[sb]-setbits[sb-1]:b-mb+1+countBitsUtil(1,b-mb);
+        return x+y+z;
+    }
+    else{
+        ll mid=(1<<(sa-1));
+        return b-a+1+countBitsUtil(a-mid+(a==mid),b-mid);
+    }
 }
 ll countBits(ll a,ll b){
     if(a<0){
@@ -28,10 +35,16 @@ ll countBits(ll a,ll b){
             return 0;
         return countBitsUtil(1,b);
     }
-    if(a>0)
-        return countBitsUtil(a,b);
+    return countBitsUtil(a,b);
+}
+void pre(){
+    setbits[0]=0;
+    for(ll i=1;i<=integer_size;i++)
+        setbits[i]=2*setbits[i-1]+(1<<(i-1));
+    return 0;
 }
 int main(void){
+    pre();
     ll t;
     scanf("%lld",&t);
     while(t--){
