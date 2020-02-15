7af998886f35059187c86f6b09f8a750bbc5c84e
ankit729 <36035184+ankit729@users.noreply.github.com>
2020-02-15 23:38:21 +0530

    Update 357. Count Numbers with Unique Digits.c

--- leetcode/357. Count Numbers with Unique Digits.c
+++ leetcode/357. Count Numbers with Unique Digits.c
@@ -8,14 +8,15 @@
 //              excluding 11,22,33,44,55,66,77,88,99
 
 int countNumbersWithUniqueDigits(int n){
+    if(n==0)
+        return 1;
+    if(n==1)
+        return 10;
     if(n>10)
         n=10;
-    int ans=1;
-    for(int i=1;i<=n;i++){
-        int p=9;
-        for(int j=1;j<i;j++)
-            p*=9-j+1;
-        ans+=p;
-    }
+    int ans=10;
+    int p=9;
+    for(int i=2;i<=n;i++)
+        ans+=(p*=9-i+2);
     return ans;
 }
