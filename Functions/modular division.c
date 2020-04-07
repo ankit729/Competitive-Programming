a073563fe5681c29e8d71a9ca6a4da706044017f
ankit729 <36035184+ankit729@users.noreply.github.com>
2020-04-07 21:13:36 +0530

    Update modular division.c

--- Functions/modular division.c
+++ Functions/modular division.c
@@ -9,17 +9,17 @@ ll gcdExtended(ll a,ll b,ll *x,ll *y){
 	*y=x1;
 	return gcd;
 }
-ll modInverse(ll b,ll m){
+ll modInverse(ll b,ll MOD){
 	ll x,y;
-	ll g=gcdExtended(b,m,&x,&y);
+	ll g=gcdExtended(b,MOD,&x,&y);
 	if(g!=1)
 		return -1;
-	return (x%m+m)%m;
+	return (x%MOD+MOD)%MOD;
 }
-ll modular_division(ll a,ll b,ll m){
-	a=a%m;
-	ll inv=modInverse(b,m);
+ll modular_division(ll a,ll b,ll MOD){
+	a=a%MOD;
+	ll inv=modInverse(b,MOD);
 	if(inv==-1)
 	    return -1;
-	return (inv*a)%m;
+	return (inv*a)%MOD;
 }
