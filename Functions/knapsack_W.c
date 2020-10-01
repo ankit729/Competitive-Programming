b4990849e383b3c9a7f74cb337588cde50e94f4e
ankit729 <36035184+ankit729@users.noreply.github.com>
2020-10-01 19:46:31 +0530

    Update knapsack_W.c

--- Functions/knapsack_W.c
+++ Functions/knapsack_W.c
@@ -1,51 +1,28 @@
 #include<stdio.h>
-#include<string.h>
-#include<stdbool.h>
-#define W_MAX 1000000001;
-int V_MAX=0;
-int dp[100001][100];
-int min(int a,int b){
-    return (a<b)?a:b;
-}
 int max(int a,int b){
     return (a>b)?a:b;
 }
-int knapsack_W_solveDp(int r,int i,int* w,int* val,int n){
-	if(r<=0)
-		return 0;
-	if(i==n)
-		return W_MAX;
-	if(dp[r][i]>=0)
-		return dp[r][i];
-	return dp[r][i]=min(knapsack_W_solveDp(r,i+1,w,val,n),w[i]+knapsack_W_solveDp(r-val[i],i+1,w,val,n));
-}
-int knapsack_W_maxWeight(int* w,int* val,int n,int W){
-    bool ispossible[V_MAX+1];
-    memset(ispossible,false,sizeof(ispossible));
-    ispossible[0]=true;
-    int M=0;
-    for(int i=0;i<n;i++)
-        for(int j=M+val[i];j>=val[i];j--)
-            if(ispossible[j-val[i]]){
-                ispossible[j]=true;
-                M=max(M,j);
-            }
-	for(int i=V_MAX;i>=0;i--)
-	    if(ispossible[i])
-		    if(knapsack_W_solveDp(i,0,w,val,n)<=W)
-                return i;
-	return 0;
-}
-void knapsack_W_pre(int n){
-    for(int i=0;i<=V_MAX;i++)
-        for(int j=0;j<n;j++)
-            dp[i][j]=-1;
+int min(int a,int b){
+    return (a<b)?a:b;
 }
 int knapsack_W(int w[],int val[],int n,int W){
+    int V_MAX=0;
     for(int i=0;i<n;i++)
         V_MAX+=val[i];
-    knapsack_W_pre(n);
-    return knapsack_W_maxWeight(w,val,n,W);
+    int minweight[V_MAX+1];
+    for(int i=1;i<=V_MAX;i++)
+        minweight[i]=W+1;
+    minweight[0]=0;
+    int M=0;
+    for(int i=0;i<n;i++)
+        for(int j=M+val[i];j>=val[i];j--){
+            minweight[j]=min(minweight[j],w[i]+minweight[j-val[i]]);
+            M=max(M,j);
+        }
+    for(int i=V_MAX;i>=0;i--)
+        if(minweight[i]<=W)
+            return i;
+	return 0;
 }
 int main(void){
 	int n,W;
