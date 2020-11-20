afe2b70bdbf0594b8996a6a9a4f1d91e51e8b7dd
ankit729 <36035184+ankit729@users.noreply.github.com>
2020-11-20 20:43:24 +0530

    Update 1006. Clumsy Factorial.cpp

--- leetcode/1006. Clumsy Factorial.cpp
+++ leetcode/1006. Clumsy Factorial.cpp
@@ -30,8 +30,8 @@
 class Solution {
 public:
     int clumsy(int N) {
-        int a[5]={-1,1,2,6,7};
+        int a[4]={1,2,6,7};
         int b[4]={1,2,2,-1};
-        return N<5?a[N]:N+b[N&3];
+        return N<5?a[N-1]:N+b[N&3];
     }
 };
