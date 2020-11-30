fa05f8fc85ebc400d0bf96840701e2a2e11accd9
ankit729 <36035184+ankit729@users.noreply.github.com>
2020-12-01 05:23:40 +0530

    Update 62. Unique Paths.c

--- leetcode/62. Unique Paths.c
+++ leetcode/62. Unique Paths.c
@@ -23,14 +23,15 @@
 // Input: m = 7, n = 3
 // Output: 28
 
-int uniquePaths(int m,int n){
-    if(m==0 || n==0)
-        return 0;
-    int dp[n];
-    for(int i=0;i<n;i++)
-        dp[i]=1;
-    for(int i=1;i<m;i++)
-        for(int j=1;j<n;j++)
-            dp[j]+=dp[j-1];
-    return dp[n-1];
-}
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if(m==0 || n==0)
+            return 0;
+        vector<int> dp(n,1);
+        for(int i=1;i<m;i++)
+            for(int j=1;j<n;j++)
+                dp[j]+=dp[j-1];
+        return dp[n-1];
+    }
+};
