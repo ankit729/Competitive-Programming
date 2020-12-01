ec0c3a30d1e1b5be2915145f31ebaff2c86fa0bf
ankit729 <36035184+ankit729@users.noreply.github.com>
2020-12-02 01:51:30 +0530

    Create 650. 2 Keys Keyboard.cpp

---
+++ leetcode/650. 2 Keys Keyboard.cpp
@@ -0,0 +1,38 @@
+// Initially on a notepad only one character 'A' is present. You can perform two operations on this notepad for each step:
+
+// Copy All: You can copy all the characters present on the notepad (partial copy is not allowed).
+// Paste: You can paste the characters which are copied last time.
+ 
+
+// Given a number n. You have to get exactly n 'A' on the notepad by performing the minimum number of steps permitted. Output the minimum number of steps to get n 'A'.
+
+// Example 1:
+
+// Input: 3
+// Output: 3
+// Explanation:
+// Intitally, we have one character 'A'.
+// In step 1, we use Copy All operation.
+// In step 2, we use Paste operation to get 'AA'.
+// In step 3, we use Paste operation to get 'AAA'.
+ 
+
+// Note:
+
+// The n will be in the range [1, 1000].
+
+class Solution {
+public:
+    int minSteps(int n) {
+        int count[n+1];
+        for(int i=2;i<=n;++i)
+            count[i]=INT_MAX;
+        count[1]=0;
+        for(int i=1;i<=n;++i){
+            int steps=count[i]+1;
+            for(int j=(i<<1);j<=n;j+=i)
+                count[j]=min(count[j],++steps);
+        }
+        return count[n];
+    }
+};
