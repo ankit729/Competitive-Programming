d72e3e580dab6b6d333defd53213ae825a34eca6
ankit729 <36035184+ankit729@users.noreply.github.com>
2020-12-03 20:47:04 +0530

    Create graphU.cpp

---
+++ Structs/graphU.cpp
@@ -0,0 +1,32 @@
+class graphU {
+    int v;
+    vector<int>* adj;
+public:
+    graphU(int V) {
+        v=V;
+        adj=new vector<int>[v];
+    }
+    void addEdge(int u,int v) {
+        adj[u].emplace_back(v);
+        adj[v].emplace_back(u);
+    }
+    bool isCyclicUtil(int& u, vector<bool>& visited, int parent) {
+        visited[u]=true;
+        for(int v:adj[u]){
+            if(!visited[v]){
+                if(isCyclicUtil(v,visited,u))
+                    return true;
+            }
+            else if(v!=parent)
+                return true;
+        }
+        return false;
+    }
+    bool isCyclic() {
+        vector<bool> visited(v,false);
+        for(int i=0;i<v;++i)
+            if(!visited[i] && isCyclicUtil(i,visited,-1))
+                return true;
+        return false;
+    }
+};
