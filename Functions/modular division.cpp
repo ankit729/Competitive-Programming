f41c86c3bd6666c934bf54f281134a4958d0cf4d
ankit729 <36035184+ankit729@users.noreply.github.com>
2020-12-08 22:06:05 +0530

    Update and rename modular division.c to modular division.cpp

---
+++ Functions/modular division.cpp
@@ -0,0 +1,25 @@
+ll gcdExtended(ll a, ll b, ll& x, ll& y) {
+	if(a==0){
+		x=0,y=1;
+		return b;
+	}
+	ll x1,y1;
+	ll gcd=gcdExtended(b%a,a,x1,y1);
+	x=y1-(b/a)*x1;
+	y=x1;
+	return gcd;
+}
+ll modInverse(ll b, ll MOD) {
+	ll x,y;
+	ll g=gcdExtended(b,MOD,x,y);
+	if(g!=1)
+		return -1;
+	return (x%MOD+MOD)%MOD;
+}
+ll modular_division(ll a, ll b, ll MOD) {
+	a=a%MOD;
+	ll inv=modInverse(b,MOD);
+	if(inv==-1)
+	    return -1;
+	return (inv*a)%MOD;
+}
