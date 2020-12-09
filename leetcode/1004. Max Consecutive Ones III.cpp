9646e156ee2e96d0d1558116db822fd36fba4fef
ankit729 <36035184+ankit729@users.noreply.github.com>
2020-12-10 00:44:46 +0530

    Update 1004. Max Consecutive Ones III.cpp

--- leetcode/1004. Max Consecutive Ones III.cpp
+++ leetcode/1004. Max Consecutive Ones III.cpp
@@ -30,20 +30,14 @@ class Solution {
 public:
     int longestOnes(vector<int>& A, int K) {
         int n=A.size();
-        int count=0;
         int l=0,r=0,ans=0;
         while(r<n){
-            if(A[r]==0){
-                while(r-l+1-count>K){
-                    if(A[l]==1)
-                        count--;
-                    l++;
-                }
-            }
-            else
-                count++;
-            ans=max(ans,r-l+1);
-            r++;
+            if(A[r++]==0)
+                K--;
+            while(K<0)
+                if(A[l++]==0)
+                    K++;
+            ans=max(ans,r-l);
         }
         return ans;
     }
