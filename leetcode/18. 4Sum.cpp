45b2a92b14e27549d338ba8608eb7a6e4599fdfd
ankit729 <36035184+ankit729@users.noreply.github.com>
2020-12-18 00:10:36 +0530

    Update 18. 4Sum.cpp

--- leetcode/18. 4Sum.cpp
+++ leetcode/18. 4Sum.cpp
@@ -23,13 +23,10 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n=nums.size();
+        int n=nums.size(),k=4;
         sort(nums.begin(),nums.end());
-        vector<vector<int>> ans;
-        if(n<=3)
-            return ans;
-        if(accumulate(nums.end()-4,nums.end(),0)<target)
-            return ans;
+        if(n<k)
+            return {};
         vector<int> start,end;
         for(int i=0;i<n;++i){
             start.push_back(i);
@@ -37,30 +34,39 @@ public:
                 i++;
             end.push_back(i);
         }
-        int size=start.size();
-        for(int ii=0;ii<size;++ii){
-            int i=start[ii];
-            if(nums[i]>target/4)
-                break;
-            for(int j=i+1,ji=(j<n && nums[i]==nums[j])?ii:ii+1;ji<size;++ji,j=(ji<size)?start[ji]:n){
-                if(nums[i]+nums[j]>target/2)
-                    break;
-                int k=j+1,l=n-1;
-                int ki=(k<n && nums[j]==nums[k])?ji:ji+1;
-                int li=size-1;
-                while(k<l){
-                    int sum=nums[i]+nums[j]+nums[k]+nums[l];
-                    if(sum==target){
-                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
-                        k=end[ki++]+1;
-                        l=start[li--]-1;
-                    }
-                    else if(sum<target)
-                        k=end[ki++]+1;
-                    else
-                        l=start[li--]-1;
-                }
+        return kSum(nums,n,start,end,start.size(),0,0,target,4);
+    }
+private:
+    vector<vector<int>> kSum(vector<int>& nums, int n, vector<int>& start, vector<int>& end, int size, int i, int ii, int target, int k) {
+        if(i>n-k || accumulate(nums.begin()+i,nums.begin()+i+k,0)>target || accumulate(nums.end()-k,nums.end(),0)<target)
+            return {};
+        if(k==2)
+            return twoSum(nums,n,start,end,size,i,ii,target);
+        vector<vector<int>> ans;
+        for(int j=i,ji=ii;ji<size;++ji,j=(ji<size)?start[ji]:n){
+            vector<vector<int>> temp=kSum(nums,n,start,end,size,j+1,(j+1<n && nums[j]==nums[j+1])?ji:ji+1,target-nums[j],k-1);
+            for(auto& x:temp){
+                ans.push_back({nums[j]});
+                ans.back().insert(ans.back().end(),x.begin(),x.end());
             }
+        }
+        return ans;
+    }
+    vector<vector<int>> twoSum(vector<int>& nums, int n, vector<int>& start, vector<int>& end, int size, int i, int ii, int target) {
+        vector<vector<int>> ans;
+        int l=i,r=n-1;
+        int li=ii,ri=size-1;
+        while(l<r){
+            int sum=nums[l]+nums[r];
+            if(sum==target){
+                ans.push_back({nums[l],nums[r]});
+                l=end[li++]+1;
+                r=start[ri--]-1;
+            }
+            else if(sum<target)
+                l=end[li++]+1;
+            else
+                r=start[ri--]-1;
         }
         return ans;
     }
