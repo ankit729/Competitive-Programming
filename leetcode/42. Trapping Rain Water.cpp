23dc7092dbc35e7aad67ecd042842e4272a0b543
ankit729 <36035184+ankit729@users.noreply.github.com>
2020-12-23 16:26:43 +0530

    Update and rename 42. Trapping Rain Water.c to 42. Trapping Rain Water.cpp

---
+++ leetcode/42. Trapping Rain Water.cpp
@@ -0,0 +1,27 @@
+// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+
+
+// The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
+
+// Example:
+
+// Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+// Output: 6
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l=0,lMax=0,r=(int)height.size()-1,rMax=0,ans=0;
+        while(l<r){
+            if(lMax<height[l])
+                lMax=height[l];
+            if(rMax<height[r])
+                rMax=height[r];
+            if(lMax<rMax)
+                ans+=lMax-height[l++];
+            else
+                ans+=rMax-height[r--];
+        }
+        return ans;
+    }
+};
