f50399cacce5512275e1588cde21c5debe9c1e31
ankit729 <36035184+ankit729@users.noreply.github.com>
2021-01-11 17:46:37 +0530

    Update 88. Merge Sorted Array.cpp

--- leetcode/88. Merge Sorted Array.cpp
+++ leetcode/88. Merge Sorted Array.cpp
@@ -21,19 +21,12 @@
 
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        m--;
-        n--;
-        int k=m+n+1;
-        while(m>=0 && n>=0){
-            if(nums1[m]>nums2[n])
-                nums1[k--]=nums1[m--];
-            else
-                nums1[k--]=nums2[n--];
-        }
-        while(m>=0)
-            nums1[k--]=nums1[m--];
-        while(n>=0)
-            nums1[k--]=nums2[n--];
+    void merge(vector<int>& nums1, int n1, vector<int>& nums2, int n2) {
+        int i=n1+n2-1;
+        n1--;n2--;
+        while(n1>=0 && n2>=0)
+            nums1[i--]=(nums1[n1]>=nums2[n2])?nums1[n1--]:nums2[n2--];
+        if(n2>=0)
+            copy(nums2.begin(),nums2.begin()+n2+1,nums1.begin());
     }
 };
