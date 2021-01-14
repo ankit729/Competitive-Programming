d3e6558e2049a690cc430ace1cc69624edb6feba
ankit729 <36035184+ankit729@users.noreply.github.com>
2021-01-14 18:29:12 +0530

    Create 102. Binary Tree Level Order Traversal.cpp

---
+++ leetcode/102. Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,51 @@
+// Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+// For example:
+// Given binary tree [3,9,20,null,null,15,7],
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// return its level order traversal as:
+// [
+//   [3],
+//   [9,20],
+//   [15,7]
+// ]
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if(!root)
+            return {};
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            ans.push_back({});
+            int n=q.size();
+            while(n--){
+                root=q.front();
+                q.pop();
+                ans.back().push_back(root->val);
+                if(root->left)
+                    q.push(root->left);
+                if(root->right)
+                    q.push(root->right);
+            }
+        }
+        return ans;
+    }
+};
