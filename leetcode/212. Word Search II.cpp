003e0ba4c762d003987fc05c070b1b20cfae406f
ankit729 <36035184+ankit729@users.noreply.github.com>
2021-01-15 19:56:29 +0530

    Update 212. Word Search II.cpp

--- leetcode/212. Word Search II.cpp
+++ leetcode/212. Word Search II.cpp
@@ -27,40 +27,104 @@
 // words[i] consists of lowercase English letters.
 // All the strings of words are unique.
 
+class TrieNode {
+public:
+    bool isEnd;
+    TrieNode* children[26];
+    TrieNode() {
+        isEnd=false;
+        for(int i=0;i<26;++i)
+            children[i]=NULL;
+    }
+};
+
+class Trie {
+    TrieNode* root;
+public:
+    Trie(vector<string>& words) {
+        root=new TrieNode();
+        for(string& word:words)
+            addWord(word);
+    }
+    TrieNode* getRoot() {
+        return root;
+    }
+    void addWord(string& word) {
+        TrieNode* curr=root;
+        for(auto& ch:word){
+            int index=ch-'a';
+            if(!curr->children[index])
+                curr->children[index]=new TrieNode();
+            curr=curr->children[index];
+        }
+        curr->isEnd=true;
+    }
+    void deleteWord(string& word) {
+        deleteWordUtil(word,word.length(),0,root);
+    }
+private:
+    bool deleteWordUtil(string& word, int n, int depth, TrieNode* curr) {
+        if(depth==n){
+            curr->isEnd=false;
+            if(isEmpty(curr)){
+                // delete(curr);
+                return true;
+            }
+            return false;
+        }
+        int index=word[depth]-'a';
+        if(deleteWordUtil(word,n,depth+1,curr->children[index])){
+            curr->children[index]=NULL;
+            if(!curr->isEnd && isEmpty(curr)){
+                // delete(curr);
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+    bool isEmpty(TrieNode* curr) {
+        for(int i=0;i<26;++i)
+            if(curr->children[i])
+                return false;
+        return true;
+    }
+};
+
 class Solution {
 public:
-    vector<vector<char>> board;
-    string word;
-    int R,C,wL;
-    vector<string> findWords(vector<vector<char>>& grid, vector<string>& s) {
-        board=grid;
-        R=board.size();
-        C=board[0].size();
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        int R=board.size(),C=(R>0)?board[0].size():0;
         vector<string> ans;
-        for(string ss:s)
-            if(exist(ss))
-                ans.push_back(ss);
+        Trie trie(words);
+        TrieNode* root=trie.getRoot();
+        for(int r=0;r<R;++r){
+            for(int c=0;c<C;++c){
+                string word;
+                helper(board,R,C,r,c,root,word,ans,trie);
+            }
+        }
         return ans;
     }
 private:
-    bool exist(string& s) {
-        word=s;
-        wL=word.length();
-        for(int i=0;i<R;++i)
-            for(int j=0;j<C;++j)
-                if(dfs(0,i,j))
-                    return true;
-        return false;
-    }
-    bool dfs(int index, int i, int j) {
-        if(index==wL)
-            return true;
-        if(i<0 || i>=R || j<0 || j>=C || board[i][j]!=word[index])
-            return false;
-        char temp=board[i][j];
-        board[i][j]='.';
-        bool ans=dfs(index+1,i-1,j) || dfs(index+1,i,j+1) || dfs(index+1,i+1,j) || dfs(index+1,i,j-1);
-        board[i][j]=temp;
-        return ans;
+    void helper(vector<vector<char>>& board, int R, int C, int r, int c, TrieNode* root, string& word, vector<string>& ans, Trie& trie) {
+        if(r<0 || r>=R || c<0 || c>=C || board[r][c]==' ')
+            return;
+        if(root->children[board[r][c]-'a']){
+            word+=board[r][c];
+            root=root->children[board[r][c]-'a'];
+            if(root->isEnd){
+                ans.push_back(word);
+                trie.deleteWord(word);
+            }
+            char ch=' ';
+            swap(ch,board[r][c]);
+            helper(board,R,C,r-1,c,root,word,ans,trie);
+            helper(board,R,C,r,c+1,root,word,ans,trie);
+            helper(board,R,C,r+1,c,root,word,ans,trie);
+            helper(board,R,C,r,c-1,root,word,ans,trie);
+            swap(ch,board[r][c]);
+            word.pop_back();
+        }
     }
 };
