a58f40c9d4086cbb32a9971fe587d75e7c4c8481
ankit729 <36035184+ankit729@users.noreply.github.com>
2021-01-17 00:45:05 +0530

    Update 692. Top K Frequent Words.cpp

--- leetcode/692. Top K Frequent Words.cpp
+++ leetcode/692. Top K Frequent Words.cpp
@@ -1,22 +1,43 @@
+// Given a non-empty list of words, return the k most frequent elements.
+
+// Your answer should be sorted by frequency from highest to lowest. If two words have the same frequency, then the word with the lower alphabetical order comes first.
+
+// Example 1:
+// Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+// Output: ["i", "love"]
+// Explanation: "i" and "love" are the two most frequent words.
+//     Note that "i" comes before "love" due to a lower alphabetical order.
+// Example 2:
+// Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+// Output: ["the", "is", "sunny", "day"]
+// Explanation: "the", "is", "sunny" and "day" are the four most frequent words,
+//     with the number of occurrence being 4, 3, 2 and 1 respectively.
+// Note:
+// You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+// Input words contain only lowercase letters.
+// Follow up:
+// Try to solve it in O(n log k) time and O(n) extra space.
+
 class Solution {
-    #define pis pair<int,string>
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string,int> mp;
+        unordered_map<string,int> count;
         for(auto& s:words)
-            mp[s]++;
-        auto compare=[&](pis& l, pis& r){
-            if(l.first==r.first)
-                return l.second>r.second;
-            return l.first<r.first;
+            count[s]++;
+        auto compare=[&](auto& a, auto& b){
+            if(a.first==b.first)
+                return a.second<b.second;
+            return a.first>b.first;
         };
-        priority_queue<pis,vector<pis>,decltype(compare)> pq(compare);
-        for(auto& x:mp)
-            pq.push({x.second,x.first});
+        priority_queue<pair<int,string>,vector<pair<int,string>>,decltype(compare)> pq(compare);
+        for(auto& x:count){
+            pq.push({move(x.second),move(x.first)});
+            if(pq.size()>k)
+                pq.pop();
+        }
         vector<string> ans(k);
-        int i=0;
         while(k--){
-            ans[i++]=pq.top().second;
+            ans[k]=move(pq.top().second);
             pq.pop();
         }
         return ans;
