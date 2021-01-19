9412b15fe670114dbd655d759735a7d037093dc2
ankit729 <36035184+ankit729@users.noreply.github.com>
2021-01-19 21:31:42 +0530

    Update longest palindromic substring.cpp

--- Functions/longest palindromic substring.cpp
+++ Functions/longest palindromic substring.cpp
@@ -5,7 +5,7 @@ public:
         int n=s.length();
         n=1+(n<<1);
         int L[n];
-        int M=1,Mi=1;
+        int M=0,Mi=0;
         for(int i=1,C=0,R=0;i<n;++i){
             L[i]=(i<R)?min(L[(C<<1)-i],R-i):0;
             while(i+L[i]<n && i-L[i]>0 && (!((i+L[i]+1)&1) || s[(i+L[i]+1)>>1]==s[(i-L[i]-1)>>1]))
