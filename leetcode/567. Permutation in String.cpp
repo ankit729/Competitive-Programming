48f9de38ef75c55de73bb4ee5f51a1fc99d24220
ankit729 <36035184+ankit729@users.noreply.github.com>
2021-01-22 20:29:04 +0530

    Update 567. Permutation in String.cpp

--- leetcode/567. Permutation in String.cpp
+++ leetcode/567. Permutation in String.cpp
@@ -21,31 +21,16 @@
 class Solution {
 public:
     bool checkInclusion(string& s1, string& s2) {
-        int n1=s1.length(),n2=s2.length();
-        if(n1>n2)
-            return false;
-        int c1[26]={},c2[26]={};
-        for(int i=0;i<n1;++i)
-            c1[s1[i]-'a']++,c2[s2[i]-'a']++;
-        int total=0;
-        for(int i=0;i<26;++i)
-            if(c1[i]==c2[i])
-                total++;
-        if(total==26)
-            return true;
-        for(int l=0,r=n1;r<n2;++l,++r){
-            int L=s2[l]-'a',R=s2[r]-'a';
-            c2[L]--;
-            if(c1[L]==c2[L])
-                total++;
-            else if(c1[L]-1==c2[L])
+        int n=s1.length();
+        int c[26]={};
+        for(auto& ch:s1)
+            c[ch-'a']++;
+        for(int l=0,r=0,total=n;s2[r];++r){
+            if(--c[s2[r]-'a']>=0)
                 total--;
-            c2[R]++;
-            if(c1[R]==c2[R])
-                total++;
-            else if(c1[R]+1==c2[R])
-                total--;
-            if(total==26)
+            while(l<=r && c[s2[l]-'a']<0)
+                c[s2[l++]-'a']++;
+            if(total==0 && r-l+1==n)
                 return true;
         }
         return false;
