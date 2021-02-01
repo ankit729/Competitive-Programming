b6db78d02643e440cb9e0fdfb8fe6d07a8b97994
ankit729 <36035184+ankit729@users.noreply.github.com>
2021-02-01 19:02:19 +0530

    Update 322. Coin Change.cpp

--- leetcode/322. Coin Change.cpp
+++ leetcode/322. Coin Change.cpp
@@ -36,16 +36,16 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(),coins.end());
         int dp[amount+1];
         fill(dp,dp+amount+1,amount+1);
         dp[0]=0;
-        for(int i=1;i<=amount;++i){
+        for(int i=coins[0];i<=amount;++i)
             for(auto& coin:coins){
                 if(coin>i)
-                    continue;
+                    break;
                 dp[i]=min(dp[i],1+dp[i-coin]);
             }
-        }
         return dp[amount]==amount+1?-1:dp[amount];
     }
 };
