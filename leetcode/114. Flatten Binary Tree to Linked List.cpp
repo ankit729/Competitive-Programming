00643f7219577f3bfc1377420e16aafc29827795
ankit729 <36035184+ankit729@users.noreply.github.com>
2021-05-14 13:59:50 +0530

    Update 114. Flatten Binary Tree to Linked List.cpp

--- leetcode/114. Flatten Binary Tree to Linked List.cpp
+++ leetcode/114. Flatten Binary Tree to Linked List.cpp
@@ -1,25 +1,31 @@
-// Given a binary tree, flatten it to a linked list in-place.
+// Given the root of a binary tree, flatten the tree into a "linked list":
 
-// For example, given the following tree:
+// The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+// The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+ 
 
-//     1
-//    / \
-//   2   5
-//  / \   \
-// 3   4   6
-// The flattened tree should look like:
+// Example 1:
 
-// 1
-//  \
-//   2
-//    \
-//     3
-//      \
-//       4
-//        \
-//         5
-//          \
-//           6
+
+// Input: root = [1,2,5,3,4,null,6]
+// Output: [1,null,2,null,3,null,4,null,5,null,6]
+// Example 2:
+
+// Input: root = []
+// Output: []
+// Example 3:
+
+// Input: root = [0]
+// Output: [0]
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 2000].
+// -100 <= Node.val <= 100
+ 
+
+// Follow up: Can you flatten the tree in-place (with O(1) extra space)?
 
 /**
  * Definition for a binary tree node.
@@ -35,21 +41,16 @@
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        dfs(root);
-    }
-private:
-    TreeNode* dfs(TreeNode* root) {
-        if(!root)
-            return NULL;
-        if(root->left){
-            TreeNode* right=root->right;
-            root->right=root->left;
-            root->left=NULL;
-            TreeNode* tail=dfs(root->right);
-            tail->right=right;
-            return (right)?dfs(right):tail;
+        while(root){
+            if(root->left){
+                TreeNode* temp=root->left;
+                while(temp->right)
+                    temp=temp->right;
+                temp->right=root->right;
+                root->right=root->left;
+                root->left=NULL;
+            }
+            root=root->right;
         }
-        return root->right?dfs(root->right):root;
     }
 };
-auto fastIO=[](){std::ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
