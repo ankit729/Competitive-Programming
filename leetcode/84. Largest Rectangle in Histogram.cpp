4192f56877125c626d398d6146e3038dcdea628b
ankit729 <36035184+ankit729@users.noreply.github.com>
2021-05-22 14:32:47 +0530

    Update 84. Largest Rectangle in Histogram.cpp

--- leetcode/84. Largest Rectangle in Histogram.cpp
+++ leetcode/84. Largest Rectangle in Histogram.cpp
@@ -1,21 +1,25 @@
-// Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+// Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
 
  
 
-
-// Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
-
- 
+// Example 1:
 
 
-// The largest rectangle is shown in the shaded area, which has area = 10 unit.
-
- 
-
-// Example:
-
-// Input: [2,1,5,6,2,3]
+// Input: heights = [2,1,5,6,2,3]
 // Output: 10
+// Explanation: The above is a histogram where width of each bar is 1.
+// The largest rectangle is shown in the red area, which has an area = 10 units.
+// Example 2:
+
+
+// Input: heights = [2,4]
+// Output: 4
+ 
+
+// Constraints:
+
+// 1 <= heights.length <= 10^5
+// 0 <= heights[i] <= 10^4
 
 class Solution {
 public:
@@ -24,24 +28,19 @@ public:
             return 0;
         int n=heights.size();
         int ans=0;
-        stack<int> stack;
-        int i=0;
-        while(i<n){
-            if(stack.empty() || heights[i]>heights[stack.top()]){
-                stack.push(i++);
-                continue;
+        stack<int> stk;
+        for(int i=0;i<n;++i){
+            while(!stk.empty() && heights[i]<heights[stk.top()]){
+                int curr=heights[stk.top()];
+                stk.pop();
+                ans=max(ans,curr*(stk.empty()?i:i-stk.top()-1));
             }
-            while(!stack.empty() && heights[i]<heights[stack.top()]){
-                int curr=heights[stack.top()];
-                stack.pop();
-                ans=max(ans,curr*(stack.empty()?i:i-stack.top()-1));
-            }
-            stack.push(i++);
+            stk.push(i);
         }
-        while(!stack.empty()){
-            int curr=heights[stack.top()];
-            stack.pop();
-            ans=max(ans,curr*(stack.empty()?i:i-stack.top()-1));
+        while(!stk.empty()){
+            int curr=heights[stk.top()];
+            stk.pop();
+            ans=max(ans,curr*(stk.empty()?n:n-stk.top()-1));
         }
         return ans;
     }
