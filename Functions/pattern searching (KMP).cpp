850702a7d2d7552d63e6d190ac4d340c010c3937
ankit729 <36035184+ankit729@users.noreply.github.com>
2022-01-21 00:13:05 +0530

    Update pattern searching (KMP).cpp

--- Functions/pattern searching (KMP).cpp
+++ Functions/pattern searching (KMP).cpp
@@ -1,38 +1,30 @@
-void computeLPSArray(string pat, int M, int* lps){
-	int len=0,i=1;
-	lps[0]=0;
-	while(i<M){
-		if(pat[i]==pat[len])
-			lps[i++]=++len;
-		else{
-			if(len!=0)
-				len=lps[len-1];
-			else 
-				lps[i++]=0;
-		}
-	}
+void make_lps(string& s, int* lps) {
+    int len=0, i=1;
+    lps[0]=0;
+    while(s[i]){
+        if(s[i]==s[len])
+            lps[i++]=++len;
+        else if(len>0)
+            len=lps[len-1];
+        else
+            lps[i++]=0;
+    }
 }
-bool KMPSearch(string pat, string txt){ 
-	int M=pat.length(),N=txt.length();
-	int lps[M];
-	computeLPSArray(pat,M,lps);
-	int i=0,j=0;
-	while(i<N){
-		if(pat[j]==txt[i]){
-			j++;
-			i++;
-		}
-		if(j==M){
-			//cout<<i-j;
-			return true;
-			j=lps[j-1];
-		}
-		else if(i<N && pat[j]!=txt[i]){
-			if(j!=0)
-				j=lps[j-1];
-			else
-				i++;
-		}
-	}
-	return false;
+
+bool kmp(string& s, string& pat) {
+    int lps[pat.length()];
+    make_lps(pat, lps);
+    int i=0, j=0;
+    while(s[i]){
+        if(s[i]==pat[j]){
+            i++, j++;
+            if(!pat[j])
+                return true;
+        }
+        else if(j>0)
+            j=lps[j-1];
+        else
+            i++;
+    }
+    return false;
 }
